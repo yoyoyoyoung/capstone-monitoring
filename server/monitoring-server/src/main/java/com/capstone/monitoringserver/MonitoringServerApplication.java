efb6eddc13d62ce776d2bd4a615f16d4ca617b31
unknown <samm1230@office.ut.ac.kr>
2026-03-09 13:39:24 +0900

    Initial commit: Project structure and .proto

---
+++ server/monitoring-server/src/main/java/com/capstone/monitoringserver/MonitoringServerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.monitoringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringServerApplication.class, args);
+    }
+
+}
